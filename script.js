1850a71ab0fc07a0771b1baf4ec010315e7b95b9
xingun <moon350703@gmail.com>
2022-02-10 16:15:21 +0700

    file

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter YourName");
+ var b=prompt("Enter Password");
+  	if(a== "nguyentuyetminh" && b=="07-10-2004")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
